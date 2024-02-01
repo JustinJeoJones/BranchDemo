fc39eac1c2cae8f59d13dbeb19762910516ddb08
Justin <Justin@grandcircus.co>
2024-02-01 11:39:43 -0500

    Updated inputs

--- BranchDemo/Program.cs
+++ BranchDemo/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//input
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
